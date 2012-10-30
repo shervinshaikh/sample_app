d2a7d9377105945f72d1f7b246a77d14616474dc
Shervin Shaikh <shervinater@gmail.com>
2012-10-29 20:57:59 -0700

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,25 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+# can write:  
+# it { should have_error_message('Invalid') }
+
+# and: 
+# describe "with valid information" do
+#  let(:user) { FactoryGirl.create(:user) }
+#  before { valid_signin(user) }
